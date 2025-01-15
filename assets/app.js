0c1aed80b0ed24a880ca810c9dcfeabffd983554
BerkeAnna <berke.anna0803@gmail.com>
2025-01-15 19:11:48 +0100

    part 3 hello

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,16 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
+
+// Hozz létre egy root elemet az új API-val
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
